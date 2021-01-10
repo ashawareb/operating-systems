3f37961602bd99ca6bcfd3fba1b4910c47bc64be
AbdUlRahmanShawareb <bzsc1010@gmail.com>
2021-01-10 12:49:05 +0200

    yl3n myteen om el RR

--- CPU-Scheduling/src/Scheduler/RoundRobin.java
+++ CPU-Scheduling/src/Scheduler/RoundRobin.java
@@ -8,99 +8,103 @@ import javafx.util.Pair;
 import java.util.*;
 
 public class RoundRobin extends Scheduler {
-    private Integer quantum;
-    private int stop;
-    private PriorityQueue<Process> burst;
-    private PriorityQueue<Process> arrival;
+    private ArrayList<Integer> burst;
+    private ArrayList<Integer> arrival;
+    private ArrayList<Integer> waiting;
     private List<Pair<Integer, String>> executionOrder;
-    private List<Process> information;
-    private Map<String, Integer> actual;
+    private Integer time;
+    private Integer quantum;
+
 
     public RoundRobin(Integer quantum, Integer contextSwitchDuration, List<Process> processes) {
         super(processes.size(), contextSwitchDuration, processes);
-        this.quantum = quantum;
-        stop = processes.size();
+        burst = new ArrayList<>();
+        arrival = new ArrayList<>();
+        waiting = new ArrayList<>();
         executionOrder = new ArrayList<>();
-        information = new ArrayList<>();
-        burst = new PriorityQueue<>(/*new BurstTimeComparator()*/new ArrivalTimeComparator());
-        arrival = new PriorityQueue<>(new ArrivalTimeComparator());
-        arrival.addAll(processes);
-        actual = new HashMap<>();
+        this.quantum = quantum;
+        this.time = 0;
         for (Process ob : processes) {
-            actual.put(ob.getProcessID(), ob.getBurstTime());
+            burst.add(ob.getBurstTime());
+            arrival.add(ob.getArrivalTime());
         }
+    }
+
+    public void waitingTimer(List<Process> processes, int quantum, int compeletiontime[]) {
+        int remainingTime[] = new int[processes.size()];
+        for (int i = 0; i < processes.size(); i++) {
+            remainingTime[i] = processes.get(i).getBurstTime();
+        }
+        //time=0;
+        int arrivaltime = 0;
+        while (true) {
+            boolean finished = true;
+            for (int i = 0; i < processes.size(); i++) {
+                if (remainingTime[i] > 0) {
+                    finished = false;
+                    if (remainingTime[i] > quantum && processes.get(i).getArrivalTime() <= arrivaltime) {
+                        time += quantum;
+                        remainingTime[i] -= quantum;
+                        arrivaltime++;
+                        executionOrder.add(new Pair<>(time, processes.get(i).getProcessID()));
+                    } else {
+                        if (processes.get(i).getArrivalTime() <= arrivaltime) {
+                            arrivaltime++;
+                            time += remainingTime[i];
+                            remainingTime[i] = 0;
+                            compeletiontime[i] = time;
+                            executionOrder.add(new Pair<>(time, processes.get(i).getProcessID()));
+                        }
+                    }
+                }
+            }
+            if (finished == true) {
+                break;
+            }
+        }
+
+    }
+
+    public void turnaroundTimer(List<Process> processes, int completiontime[]) {
+        for (int i = 0; i < processes.size(); i++) {
+            int tat = completiontime[i] - processes.get(i).getArrivalTime();
+            processes.get(i).setTurnaroundTime(tat);
+            int wt = processes.get(i).getTurnaroundTime() - processes.get(i).getBurstTime();
+            processes.get(i).setWaitingTime(wt);
+        }
+    }
+
+    public void avaregeTimer(List<Process> processes, int quantum) {
+        int completionTime[] = new int[processes.size()];
+        waitingTimer(processes, quantum, completionTime);
+        turnaroundTimer(processes, completionTime);
+        out(processes);
     }
 
     @Override
     public void schedule() {
-        Process p = null;
-        for (int i = 0; i <= 250000; i++) {
-            while (!arrival.isEmpty() && arrival.peek().getArrivalTime() <= i) {
-                Process arrived = arrival.poll(); //get the process in the turn and remove it from the arrival
-                burst.add(arrived);
-            }
-            if (burst.isEmpty()) {
-                continue;
-            }
-            /*if (p != null) {
-                if (!p.getProcessID().equals(burst.peek().getProcessID()) && p.getBurstTime() > 0) {
-                    i += contextSwitchDuration;
-                }
-            }*/
-            p = burst.poll();
-            executionOrder.add(new Pair<>(i, p.getProcessID()));
-            //p.setBurstTime(p.getBurstTime() - quantum);
-            if (p.getBurstTime() > quantum) {
-                int pBurstTime = p.getBurstTime();
-                pBurstTime -= quantum;
-                p.setBurstTime(pBurstTime);
-                i += quantum + contextSwitchDuration;
-                burst.add(p);
-            } else {
-                stop--;
-                i += (quantum - p.getBurstTime()) + contextSwitchDuration;
-                p.setTurnaroundTime(i - p.getArrivalTime());
-                p.setWaitingTime(p.getTurnaroundTime() - actual.get(p.getProcessID()));
-                information.add(p);
-                if (stop == 0) {
-                    break;
-                }
-            }
-            /*if (p.getBurstTime() > 0) {
-                burst.add(p);
-            } else {
-                stop--;
-                int pWaitingTime = i - p.getArrivalTime() - actual.get(p.getProcessID()) + 1;
-                int pTurnaroundTime = i - p.getArrivalTime() + 1;
-                p.setWaitingTime(pWaitingTime);
-                p.setTurnaroundTime(pTurnaroundTime);
-                information.add(p);
-                i += contextSwitchDuration;
-                if (stop == 0) {
-                    break;
-                }
-            }*/
-        }
-        out();
+        avaregeTimer(processes, quantum);
     }
 
-    private void out() {
+    private void out(List<Process> information) {
         Double avaregeWaitingTime = 0.0;
         Double avaregeTurnaroundTime = 0.0;
-        System.out.println("Tim, \t Process processing");
+        System.out.println("Time \t Process processing");
         for (Pair<Integer, String> e : executionOrder) {
             System.out.println(e.getKey() + " \t\t " + e.getValue());
         }
         System.out.println("+-------------------------------------------------+");
         for (Process ob : information) {
-            System.out.println("| Process name:               |   " + ob.getProcessName() + "              |");
-            System.out.println("| Process ID:                 |   " + ob.getProcessID() + "              |");
+            System.out.println("| Process name:               |   " + ob.getProcessName() + "               |");
+            System.out.println("| Process ID:                 |   " + ob.getProcessID() + "               |");
             System.out.println("| Process waiting time:       |   " + ob.getWaitingTime() + "               |");
             System.out.println("| Process turnaround time:    |   " + ob.getTurnaroundTime() + "               |");
             System.out.println("+-------------------------------------------------+");
-            avaregeWaitingTime += ob.getWaitingTime() * 1.0 / information.size();
-            avaregeTurnaroundTime += ob.getTurnaroundTime() * 1.0 / information.size();
+            avaregeWaitingTime += ob.getWaitingTime() * 1.0;
+            avaregeTurnaroundTime += ob.getTurnaroundTime() * 1.0;
         }
+        avaregeTurnaroundTime /= information.size();
+        avaregeWaitingTime /= information.size();
         System.out.println("+-------------------------------------------------+");
         System.out.println("| Avarege Waiting time:       |   " + avaregeWaitingTime + "             |");
         System.out.println("| Avarege Turnaround time:    |   " + avaregeTurnaroundTime + "             |");
